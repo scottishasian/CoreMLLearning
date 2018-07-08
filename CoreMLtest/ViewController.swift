2d8f34224fd3a03131ce3a55a423c8b561c769f1
Kynan <k.song@blueyonder.co.uk>
2018-07-08 14:58:33 +0100

    Starting CoreML learning

--- CoreMLtest/ViewController.swift
+++ CoreMLtest/ViewController.swift
@@ -4,15 +4,28 @@
 //
 //  Created by Kynan Song on 08/07/2018.
 //  Copyright © 2018 Kynan Song. All rights reserved.
-//
+//  Learning from Brian Voong youtube video.
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        let captureSession = AVCaptureSession()
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        //if device has no camera use try with a ? to avoid errors and guard statements to unwrap the optionals.
+        //Need to set privacy settings in info.plist
+        
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
