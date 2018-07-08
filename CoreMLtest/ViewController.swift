864c3f2424f61a9190e8678c42bafb8199c4f0f1
Kynan <k.song@blueyonder.co.uk>
2018-07-08 15:58:13 +0100

    resNet50 library added and basic recognition complete

--- CoreMLtest/ViewController.swift
+++ CoreMLtest/ViewController.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 import AVKit
+import Vision
+//for image recognition
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Shows the camera/ starts up the camera.
         guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
         let captureSession = AVCaptureSession()
+        
+        captureSession.sessionPreset = .photo
+        //Will crop the view frame.
         
         guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
         //if device has no camera use try with a ? to avoid errors and guard statements to unwrap the optionals.
@@ -24,8 +30,62 @@ class ViewController: UIViewController {
         captureSession.addInput(input)
         captureSession.startRunning()
         
+        //Output preview layer
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        //Add as sublayer to view controllers view.
+        //Need to specify a frame for the preview layer/
+        previewLayer.frame = view.frame
+        //Activates the camera on view.
+        
+        //Access to camera frame layer.
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "VideoQueue"))
+        //Needs a custom queue for the data. and self on the delegate as it is in the view controller.
         
         
+        //Add to capture sessions.
+        captureSession.addOutput(dataOutput)
+        
+        //Analyze camera data.
+    
+       // VNImageRequestHandler(cgImage: <#T##CGImage#>, options: [:]).perform(<#T##requests: [VNRequest]##[VNRequest]#>)
+        
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput  sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        //print("Camera captured a frame", Date())
+        //Called everytime the camera captures a frame
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
+        //Needs to be unwrapped.
+        
+        guard let resNetModel = try? VNCoreMLModel(for: Resnet50().model) else {return }
+
+        //Go to developer.apple.com/machine learning to find the right model
+        //Using resNet50 Model, needs to be wrapped in VNCoreModel
+        
+        let request = VNCoreMLRequest(model: resNetModel) {
+            (finishedRequest, error) in
+
+            //cast results into an array.
+            
+            guard let resultsArray = finishedRequest.results as? [VNClassificationObservation] else {return}
+            //VNCLassificationObservation is the object data.
+            
+            guard let firstObservation = resultsArray.first else {return}
+            //What the camera thinks the object is.
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            
+        }
+
+        
+        //Helps access VNImage request handler
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        //Execute request and tell us what the object is.
     }
 
     override func didReceiveMemoryWarning() {
